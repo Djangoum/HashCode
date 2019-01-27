c3fad1649982989b348cf0782ff49aa9894728a7
Jesus Zaplana <jesuszaplana@gmail.com>
2019-01-27 10:42:19 +0100

    Work in progress

--- Jesus/slices.hs
+++ Jesus/slices.hs
@@ -1,9 +1,1 @@
-main :: IO ()
-main = do
-    putStrLn "Hello world!"
-    -- prod = 2 * 3
-    -- putStrLn prod
-
--- sliceTypes' = [ (a,b) | a <- [1..6], b <- [1..6], a * b <= 6, a * b >= 2 ]
--- sliceTypes'
--- [ (a,b) | a <- [1..h], b <- [1..h], a * b <= h, a * b >= 2 * l ]
+sliceTypes a b = [ (x,y) | x <- [1..b], y <- [1..b], x*y <= b, x*y >= 2*a]
